2c2a10485707c079eae86af85c1ea890de9d01bb
FCO-1 <pacomendez012@gmail.com>
2021-11-11 09:00:17 -0600

    cambios en mix

--- mix.exs
+++ mix.exs
@@ -41,6 +41,7 @@ defmodule TemplateExp.MixProject do
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:jason, "~> 1.0"},
+
       {:plug_cowboy, "~> 2.0"}
     ]
   end
